e43cc400394c797e41a2a348f27c18dd7d9453a6
ariefya3 <muh.arief.yae@gmail.com>
2020-10-07 20:36:44 +0700

    versi 1.0.0

---
+++ application/views/home/footer.php
@@ -0,0 +1,11 @@
+<!-- Scripts -->
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/typewriter.js"></script>
+	<script src="assets/js/jquery.onepagenav.js"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+
+</html>
